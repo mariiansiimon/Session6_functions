6e09f1ef44d2752bc358a7a04420db2b5dab5b76
Marian Simon <93652525+mariiansiimon@users.noreply.github.com>
2023-01-16 20:06:12 +0330

    Add files via upload

---
+++ 4_func.py
@@ -0,0 +1,11 @@
+# تابعی بنویسید که فاصله اقلیدسی دو نقطه a,b را در فضای دو بعدی حساب کند.
+
+import math
+
+def edu_distance( a1, b1, a2, b2 ):
+    d = ( a2 - a1 ) ** 2 - ( b2 - b1 ) ** 2 
+    final_d = math.sqrt(d)
+    
+    return print(final_d)
+
+edu_distance(30, 12 , 6, 2)
